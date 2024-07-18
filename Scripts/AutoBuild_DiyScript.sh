8b2aa405b4b2976d68758a43b17418ab680adba7
zhiern <168670760+zhiern@users.noreply.github.com>
2024-07-18 16:08:11 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -31,7 +31,14 @@ Firmware_Diy_Core() {
 }
 
 Firmware_Diy() {
-
+        sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+        sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+        ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+        rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+        rm -rf feeds/packages/utils/v2dat
+        rm -rf feeds/packages/lang/golang
+        git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+        ./scripts/feeds install -a 
 	# 请在该函数内定制固件
 
 	# 可用预设变量, 其他可用变量请参考运行日志
