552c58e3437da78e370aa6f5a9a183dd49db6455
zhiern <168670760+zhiern@users.noreply.github.com>
2024-07-18 16:25:55 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -36,6 +36,8 @@ Firmware_Diy() {
         ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
         rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
         rm -rf feeds/packages/utils/v2dat
+	rm -rf feeds/luci/themes/luci-theme-argon
+        rm -rf feeds/luci/themes/luci-theme-argon-mod
         rm -rf feeds/packages/lang/golang
         git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
         ./scripts/feeds install -a 
