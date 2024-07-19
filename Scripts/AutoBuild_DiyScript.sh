241315d8d2360cd752a81b1668ecde4ac03f8752
zhiern <168670760+zhiern@users.noreply.github.com>
2024-07-19 15:59:43 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -89,15 +89,15 @@ Firmware_Diy() {
         git_sparse_clone main https://github.com/linkease/istore luci
 	# 取消主题默认设置
         find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
-	./scripts/feeds update -a
-        ./scripts/feeds install -a
         # 更改 Argon 主题背景
         cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 	# x86 型号只显示 CPU 型号
         sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
 	# 修改本地时间格式
         sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
-
+	# 下载并更新
+        ./scripts/feeds update -a
+        ./scripts/feeds install -a
 	# 请在该函数内定制固件
 
 	# 可用预设变量, 其他可用变量请参考运行日志
