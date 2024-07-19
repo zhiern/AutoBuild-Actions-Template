63eab532ea3ad59b05f2e3927b567b359e6eaae0
zhiern <168670760+zhiern@users.noreply.github.com>
2024-07-19 10:03:58 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -31,24 +31,66 @@ Firmware_Diy_Core() {
 }
 
 Firmware_Diy() {
-	rm -rf feeds/luci/themes/luci-theme-argon
-        rm -rf feeds/luci/themes/luci-theme-argon-mod
-        rm -rf feeds/packages/lang/golang
-        find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-        find ./ | grep Makefile | grep mosdns | xargs rm -f
-        git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-        git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-	git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-        git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/downloads/luci-theme-argon
-	git clone -b main https://github.com/oppen321/openclash package/openclash
-        git clone -b main https://github.com/linkease/istore-ui package/istoreos-ui
-        git clone -b main https://github.com/linkease/istore package/istoreos
-	git clone -b main https://github.com/kenzok8/golang feeds/packages/lang/golang 
-        git clone -b main https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-package
-	git clone -b main https://github.com/xiaorouji/openwrt-passwall package/passwall
-        git clone -b main https://github.com/xiaorouji/openwrt-passwall2 package/passwall2
-	git clone -b main https://github.com/oppen321/luci-app-adguardhome package/luci-app-adguardhome
-        git clone -b master https://github.com/fw876/helloworld package/helloword
+	# 移除要替换的包
+        rm -rf feeds/packages/net/mosdns
+        rm -rf feeds/packages/net/msd_lite
+        rm -rf feeds/packages/net/smartdns
+        rm -rf feeds/luci/themes/luci-theme-argon
+        rm -rf feeds/luci/themes/luci-theme-netgear
+        rm -rf feeds/luci/applications/luci-app-mosdns
+        rm -rf feeds/luci/applications/luci-app-netdata
+        rm -rf feeds/luci/applications/luci-app-serverchan
+        # Git稀疏克隆，只克隆指定目录到本地
+        function git_sparse_clone() {
+         branch="$1" repourl="$2" && shift 2
+         git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+         repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+         cd $repodir && git sparse-checkout set $@
+         mv -f $@ ../package
+         cd .. && rm -rf $repodir
+        }
+	# 添加额外插件
+        git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+        git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
+        git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
+        git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+        git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+        git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
+        git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
+        git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/luci-app-eqos
+        # 科学上网插件
+        git clone --depth=1 -b master https://github.com/fw876/helloworld package/luci-app-ssr-plus
+        git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+        git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+        git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+        git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
+        # Themes
+        git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
+        git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+        git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+        git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
+        git_sparse_clone main https://github.com/haiibo/packages luci-theme-atmaterial luci-theme-opentomcat luci-theme-netgear
+        # SmartDNS
+        git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+        git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
+        # msd_lite
+        git clone --depth=1 https://github.com/ximiTech/luci-app-msd_lite package/luci-app-msd_lite
+        git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
+	# MosDNS
+        git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+        # Alist
+        git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
+        # DDNS.to
+        git_sparse_clone main https://github.com/linkease/nas-packages-luci luci/luci-app-ddnsto
+        git_sparse_clone master https://github.com/linkease/nas-packages network/services/ddnsto
+        # iStore
+        git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
+        git_sparse_clone main https://github.com/linkease/istore luci
+	# 取消主题默认设置
+        find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
+	./scripts/feeds update -a
+        ./scripts/feeds install -a
+
 	# 请在该函数内定制固件
 
 	# 可用预设变量, 其他可用变量请参考运行日志
